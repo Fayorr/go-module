6ac57b0ae839f61a22049d596249a829749e2d30
fosho <fayokunmiosho@gmail.com>
2026-03-04 16:03:02 +0100

    ascii art

---
+++ ascii-art/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func validator(arguments []string) bool {
+
+	if len(arguments) < 1 {
+		return false
+	}
+	if arguments[0] == "" {
+		return false
+	}
+
+	return true
+}
+func readFile() (string, error) {
+	content, err := os.ReadFile("standard.txt")
+
+	if err != nil {
+		fmt.Println("Error in reading file")
+		return "", err
+	}
+
+	return string(content), nil
+}
+func main() {
+	arguments := os.Args[1:]
+
+	if !validator(arguments) {
+		return
+	}
+
+	result, _ := readFile()
+
+	trimmedResult := strings.Split(result, "\n")
+
+	word := strings.Join(arguments, " ")
+	wordSlice := strings.Split(word, "\\n")
+	fmt.Println(wordSlice)
+	for j := 0; j < len(wordSlice); j++ {
+		for i := 1; i <= 8; i++ {
+			for _, ch := range wordSlice[j] {
+				pos := (int(ch-' ') * 9)
+				fmt.Print(trimmedResult[pos+i])
+			}
+			fmt.Println()
+		}
+	}
+
+}
