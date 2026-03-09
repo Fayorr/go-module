71017a5a48342d992d7f77f1f7adfe03fe15a4e2
fosho <fayokunmiosho@gmail.com>
2026-03-09 17:49:13 +0100

    add a test file still in development

--- ascii-art-color/runner.go
+++ ascii-art-color/runner.go
@@ -4,27 +4,38 @@ import (
 	"strings"
 )
 
-// func colors() {
-// 	color := map[string]string{
-// 		"reset":   "\033[0m",
-// 		"red":     "\033[31m",
-// 		"green":   "\033[32m",
-// 		"yellow":  "\033[33m",
-// 		"blue":    "\033[34m",
-// 		"magenta": "\033[35m",
-// 		"cyan":    "\033[36m",
-// 		"gray":    "\033[37m",
-// 		"white":   "\033[97m",
-// 	}
-
-// }
-
 func Runner(result, flag, subString string, sentence string) string {
+
+	colorDict := map[string]string{
+		"reset":   "\033[0m",
+		"red":     "\033[31m",
+		"green":   "\033[32m",
+		"yellow":  "\033[33m",
+		"blue":    "\033[34m",
+		"magenta": "\033[35m",
+		"purple":  "\033[35m",
+		"cyan":    "\033[36m",
+		"gray":    "\033[37m",
+		"white":   "\033[97m",
+		"orange":  "\033[38;5;208m",
+
+		// --- HEX CODES ---
+		"#000000": "\033[30m",       // Black
+		"#ff0000": "\033[31m",       // Red
+		"#00ff00": "\033[32m",       // Green
+		"#ffff00": "\033[33m",       // Yellow
+		"#0000ff": "\033[34m",       // Blue
+		"#ff00ff": "\033[35m",       // Magenta
+		"#00ffff": "\033[36m",       // Cyan
+		"#ffffff": "\033[37m",       // White
+		"#ffa500": "\033[38;5;208m", // Orange
+
+	}
 
 	colorName := strings.TrimPrefix(flag, "--color=")
 
-	for _, ch := range 
-
+	ansiColor := colorDict[colorName]
+	ansiReset := colorDict["reset"]
 
 	trimmedResult := strings.Split(result, "\n")
 
@@ -36,10 +47,35 @@ func Runner(result, flag, subString string, sentence string) string {
 			finalString.WriteRune('\n')
 			continue
 		}
+		// define the map with length of current word in sentence
+		colorMap := make([]bool, len(wordSlice[j]))
+
+		// check if theres a subString and colours everything if there isn't
+		if subString == "" {
+			for k := 0; k < len(colorMap); k++ {
+				colorMap[k] = true
+			}
+		} else {
+			for k := 0; k < len(wordSlice[j]); k++ {
+				if strings.HasPrefix(wordSlice[j][k:], subString) {
+					for l := 0; l < len(subString); l++ {
+						colorMap[k+l] = true
+					}
+				}
+			}
+		}
+
 		for i := 1; i <= 8; i++ {
-			for _, ch := range wordSlice[j] {
+			for index, ch := range wordSlice[j] {
 				pos := (int(ch-' ') * 9)
+				if colorMap[index] == true {
+					finalString.WriteString(ansiColor)
+				}
 				finalString.WriteString(trimmedResult[pos+i])
+
+				if colorMap[index] == true {
+					finalString.WriteString(ansiReset)
+				}
 			}
 			finalString.WriteRune('\n')
 		}
--- ascii-art-color/runner_test.go
+++ ascii-art-color/runner_test.go
@@ -11,29 +11,14 @@ func TestRun(t *testing.T) {
 		result   string
 	}{
 		{
-			name: "hello\\n\\nthere",
-			expected: ` _              _   _          
-| |            | | | |         
-| |__     ___  | | | |   ___   
-|  _ \   / _ \ | | | |  / _ \  
-| | | | |  __/ | | | | | (_) | 
-|_| |_|  \___| |_| |_|  \___/  
-                               
-                               
-
- _     _                           
-| |   | |                          
-| |_  | |__     ___   _ __    ___  
-| __| |  _ \   / _ \ | '__|  / _ \ 
-\ |_  | | | | |  __/ | |    |  __/ 
- \__| |_| |_|  \___| |_|     \___| 
-                                   
-                                   
-`,
+			name:     "hello world",
+			expected: `--color=red "hello world"`,
+			result: ``,
 		},
 		{
 			name: "{Hello & There #}",
-			expected: `   __  _    _          _   _                                _______   _                                    _  _    __    
+			expected: "--color=red {Hello & There #}"
+			result: `   __  _    _          _   _                                _______   _                                    _  _    __    
   / / | |  | |        | | | |                 ___          |__   __| | |                                 _| || |_  \ \   
  | |  | |__| |   ___  | | | |   ___          ( _ )            | |    | |__     ___   _ __    ___        |_  __  _|  | |  
 / /   |  __  |  / _ \ | | | |  / _ \         / _ \/\          | |    |  _ \   / _ \ | '__|  / _ \        _| || |_    \ \ 
@@ -47,12 +32,11 @@ func TestRun(t *testing.T) {
 
 	content, _ := readFile()
 
-
 	for _, tt := range tests {
-		t.Run(tt.name,  func(t *testing.T) {
-			got := Runner(content, tt.name)
-			if got != tt.expected {
-				t.Errorf("Expected:\n%q\n\nGot:\n%q", tt.expected, got)
+		t.Run(tt.name, func(t *testing.T) {
+			got := Runner(content, tt.flag, tt.subString, tt.sentence)
+			if got != tt.result {
+				t.Errorf("Expected:\n%q\n\nGot:\n%q", tt.result, got)
 			}
 		})
 	}
