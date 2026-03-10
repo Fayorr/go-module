9b297a3f64e902cbc27fc7caed82618cad357002
Fayorr <fayokunmiosho@gmail.com>
2026-03-10 22:30:22 +0100

    add test files for 7 audits

--- ascii-art-color/runner_test.go
+++ ascii-art-color/runner_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
+
 // 2nOs3HqZd5
 func TestRun(t *testing.T) {
 	tests := []struct {
@@ -17,56 +19,49 @@ func TestRun(t *testing.T) {
 			flag:      "--color=red",
 			subString: "",
 			sentence:  "hello world",
-			expected:  "", // Replace with actual output or Golden File
+			expected:  "./test-files/audit1.txt", // Replace with actual output or Golden File
 		},
 		{
 			name:      "Audit 2: Color everything green",
 			flag:      "--color=green",
 			subString: "",
 			sentence:  "1 + 1 = 2",
-			expected:  "",
+			expected:  "./test-files/audit2.txt",
 		},
 		{
 			name:      "Audit 3: Color everything yellow",
 			flag:      "--color=yellow",
 			subString: "",
 			sentence:  "(%&) ??",
-			expected:  "",
+			expected:  "./test-files/audit3.txt",
 		},
 		{
 			name:      "Audit 4: Color second until the last letter",
 			flag:      "--color=cyan", // The audit didn't specify a color, so we use cyan
-			subString: "ello",
-			sentence:  "hello",
-			expected:  "",
+			subString: "RNIGERDACT",
+			sentence:  "MRNIGERDACT",
+			expected:  "./test-files/audit4.txt",
 		},
 		{
 			name:      "Audit 5: Color the second letter",
 			flag:      "--color=magenta", // Using magenta as a placeholder
 			subString: "e",
 			sentence:  "hello",
-			expected:  "",
+			expected:  "./test-files/audit5.txt",
 		},
 		{
 			name:      "Audit 6: Color just two letters",
 			flag:      "--color=white", // Using white as a placeholder
-			subString: "ll",
-			sentence:  "hello",
-			expected:  "",
+			subString: "ii",
+			sentence:  "ascii",
+			expected:  "./test-files/audit6.txt",
 		},
 		{
 			name:      "Audit 7: Color specific word with different casing",
 			flag:      "--color=orange",
 			subString: "GuYs",
 			sentence:  "HeY GuYs",
-			expected:  "",
-		},
-		{
-			name:      "Audit 8: Color single uppercase letter in symbols",
-			flag:      "--color=blue",
-			subString: "B",
-			sentence:  "RGB()",
-			expected:  "",
+			expected:  "./test-files/audit7.txt",
 		},
 	}
 
@@ -75,8 +70,15 @@ func TestRun(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Runner(content, tt.flag, tt.subString, tt.sentence)
-			if got != tt.result {
-				t.Errorf("Expected:\n%q\n\nGot:\n%q", tt.result, got)
+			expectedBytes, err := os.ReadFile(tt.expected)
+
+			if err != nil {
+				t.Errorf("Failed to Read File: %v", err)
+			}
+			expectedString := string(expectedBytes)
+
+			if got != expectedString {
+				t.Errorf("Expected:\n%q\n\nGot:\n%q", expectedString, got)
 			}
 		})
 	}
