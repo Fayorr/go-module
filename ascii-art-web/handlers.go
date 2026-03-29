ec5965ec7a5e50c638456c75d4b990318df23287
Fayorr <fayokunmiosho@gmail.com>
2026-03-29 18:00:16 +0100

    fix final version 1.0.0

--- ascii-art-web/handlers.go
+++ ascii-art-web/handlers.go
@@ -16,6 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 		http.NotFound(w, r)
+		return
 	}
 	err = ts.ExecuteTemplate(w, "index", nil)
 	if err != nil {
@@ -25,15 +26,39 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func displayArt(w http.ResponseWriter, r *http.Request) {
+	
 	sentence := r.FormValue("textInput")
 	banner := r.FormValue("bannerType")
+	// fmt.Printf("Raw received string: %q\n", sentence)
+	// 1. Check for missing data
+	if sentence == "" {
+		http.Error(w, "400 Bad Request - Invalid Input", http.StatusBadRequest)
+	}
+
+// 2. Validationm of the banner type
+if banner != "standard" && banner != "shadow" && banner != "thinkertoy" || banner == ""{
+    http.Error(w, "400 Bad Request - Invalid Banner", http.StatusBadRequest)
+    return
+}
+
+// 3. Loop through every individual character in the sentence
+for _, char := range sentence {
+    // If the character is NOT a newline (10) AND NOT a carriage return (13)
+    // AND it falls outside the standard 32-126 range...
+    if char != '\n' && char != '\r' && (char < 32 || char > 126) {
+        http.Error(w, "400 Bad Request - Non-ASCII Character Detected", http.StatusBadRequest)
+        return 
+    }
+}
 	log.Print(banner)
 	result := Runner(sentence, banner)
 
 	tmpl, err := template.ParseFiles("./templates/ascii.html")
 
 	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		log.Println(err.Error())
+		return
 	}
 	data := struct {
 		Result string
--- ascii-art-web/runner.go
+++ ascii-art-web/runner.go
@@ -30,7 +30,7 @@ func Runner(sentence, banner string) string {
 
 	trimmedResult := strings.Split(string(result), "\n")
 
-	wordSlice := strings.Split(sentence, "\\n")
+	wordSlice := strings.Split(sentence, "\r\n")
 
 	var finalString strings.Builder
 	for j := 0; j < len(wordSlice); j++ {
@@ -39,7 +39,7 @@ func Runner(sentence, banner string) string {
 			continue
 		}
 
-		for i := 0; i < 8; i++ {
+		for i := 1; i <= 8; i++ {
 			for _, ch := range wordSlice[j] {
 				pos := (int(ch-' ') * 9)
 				finalString.WriteString(trimmedResult[pos+i])
