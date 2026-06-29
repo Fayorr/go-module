4569607588a27f2228fb7de8e5d79ff675fdbfbd
fosho <fayokunmiosho@gmail.com>
2026-06-29 16:36:03 +0100

    add: tour location and new styles

--- groupie-tracker/config/api.go
+++ groupie-tracker/config/api.go
@@ -4,3 +4,4 @@ const ARTIST_URL_API = "https://groupietrackers.herokuapp.com/api/artists"
 const LOCATION_URL_API = "https://groupietrackers.herokuapp.com/api/locations"
 const DATES_URL_API = "https://groupietrackers.herokuapp.com/api/dates"
 const RELATIONS_URL_API = "https://groupietrackers.herokuapp.com/api/relation"
+
--- groupie-tracker/go.mod
+++ groupie-tracker/go.mod
@@ -23,6 +23,6 @@ require (
 	github.com/spf13/cast v1.9.2 // indirect
 	github.com/tdewolff/parse/v2 v2.8.3 // indirect
 	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/text v0.38.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
 )
